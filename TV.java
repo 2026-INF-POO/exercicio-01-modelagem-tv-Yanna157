c1bc85567fe8e17d61231bb25a3e46f88d8814e7
Yanna157 <20251ti0048@ifba.edu.br>
2026-03-03 09:04:21 -0300

    modelagem de TV

--- TV.java
+++ TV.java
@@ -1,1 +1,35 @@
+class TV {
+  int tamanho;
+  int voltagem;
+  int canal;
+  int volume = 5;
 
+ int aumentarVolume(){
+     if(volume < 10){
+       volume++;
+  }
+   return volume;
+ }
+
+  void desligar(){
+    //lógica para TV desligar
+  }
+
+  void aumentar e diminuir o volume(){
+    //lógica para TV aumentar e diminuir o volume
+  }
+
+  void subir_de_canal(){
+    //lógica para TV trocar de canal (subindo)
+  }
+
+  void descer_de_canal(){
+    //lógica para TV trocar de canal (descendo)
+  }
+
+
+
+ 
+  
+
+}
